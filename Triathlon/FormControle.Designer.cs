a5d8fc62664195452549788ac0d6ad97efd73163
Alexis Maris <alextpssio@gmail.com>
2024-04-24 09:30:09 +0200

    Mise En place formulaire dopage et triathlon

--- Triathlon/FormControle.Designer.cs
+++ Triathlon/FormControle.Designer.cs
@@ -30,8 +30,19 @@
         {
             this.tabControleDopage = new System.Windows.Forms.TabControl();
             this.tabPageAffDopage = new System.Windows.Forms.TabPage();
+            this.dataGridDopage = new System.Windows.Forms.DataGridView();
             this.tabPageDetails = new System.Windows.Forms.TabPage();
+            this.lblCodeProduit = new System.Windows.Forms.Label();
+            this.lblIdLabo = new System.Windows.Forms.Label();
+            this.lblNumTriathlon = new System.Windows.Forms.Label();
+            this.lblNumTriathlete = new System.Windows.Forms.Label();
+            this.lblPrélèvement = new System.Windows.Forms.Label();
+            this.lblVerif = new System.Windows.Forms.Label();
+            this.lblId = new System.Windows.Forms.Label();
             this.tabControleDopage.SuspendLayout();
+            this.tabPageAffDopage.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridDopage)).BeginInit();
+            this.tabPageDetails.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControleDopage
@@ -46,6 +57,7 @@
             // 
             // tabPageAffDopage
             // 
+            this.tabPageAffDopage.Controls.Add(this.dataGridDopage);
             this.tabPageAffDopage.Location = new System.Drawing.Point(4, 25);
             this.tabPageAffDopage.Name = "tabPageAffDopage";
             this.tabPageAffDopage.Padding = new System.Windows.Forms.Padding(3);
@@ -54,8 +66,25 @@
             this.tabPageAffDopage.Text = "Affichage Contrôle";
             this.tabPageAffDopage.UseVisualStyleBackColor = true;
             // 
+            // dataGridDopage
+            // 
+            this.dataGridDopage.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridDopage.Location = new System.Drawing.Point(7, 7);
+            this.dataGridDopage.Name = "dataGridDopage";
+            this.dataGridDopage.RowHeadersWidth = 51;
+            this.dataGridDopage.RowTemplate.Height = 24;
+            this.dataGridDopage.Size = new System.Drawing.Size(755, 384);
+            this.dataGridDopage.TabIndex = 0;
+            // 
             // tabPageDetails
             // 
+            this.tabPageDetails.Controls.Add(this.lblCodeProduit);
+            this.tabPageDetails.Controls.Add(this.lblIdLabo);
+            this.tabPageDetails.Controls.Add(this.lblNumTriathlon);
+            this.tabPageDetails.Controls.Add(this.lblNumTriathlete);
+            this.tabPageDetails.Controls.Add(this.lblPrélèvement);
+            this.tabPageDetails.Controls.Add(this.lblVerif);
+            this.tabPageDetails.Controls.Add(this.lblId);
             this.tabPageDetails.Location = new System.Drawing.Point(4, 25);
             this.tabPageDetails.Name = "tabPageDetails";
             this.tabPageDetails.Padding = new System.Windows.Forms.Padding(3);
@@ -63,6 +92,69 @@
             this.tabPageDetails.TabIndex = 1;
             this.tabPageDetails.Text = "Détails";
             this.tabPageDetails.UseVisualStyleBackColor = true;
+            // 
+            // lblCodeProduit
+            // 
+            this.lblCodeProduit.AutoSize = true;
+            this.lblCodeProduit.Location = new System.Drawing.Point(147, 247);
+            this.lblCodeProduit.Name = "lblCodeProduit";
+            this.lblCodeProduit.Size = new System.Drawing.Size(138, 16);
+            this.lblCodeProduit.TabIndex = 6;
+            this.lblCodeProduit.Text = "Code produit dépisté :";
+            // 
+            // lblIdLabo
+            // 
+            this.lblIdLabo.AutoSize = true;
+            this.lblIdLabo.Location = new System.Drawing.Point(147, 211);
+            this.lblIdLabo.Name = "lblIdLabo";
+            this.lblIdLabo.Size = new System.Drawing.Size(90, 16);
+            this.lblIdLabo.TabIndex = 5;
+            this.lblIdLabo.Text = "Id Laboratoire";
+            // 
+            // lblNumTriathlon
+            // 
+            this.lblNumTriathlon.AutoSize = true;
+            this.lblNumTriathlon.Location = new System.Drawing.Point(147, 176);
+            this.lblNumTriathlon.Name = "lblNumTriathlon";
+            this.lblNumTriathlon.Size = new System.Drawing.Size(135, 16);
+            this.lblNumTriathlon.TabIndex = 4;
+            this.lblNumTriathlon.Text = "Numéro de Triathlon :";
+            // 
+            // lblNumTriathlete
+            // 
+            this.lblNumTriathlete.AutoSize = true;
+            this.lblNumTriathlete.Location = new System.Drawing.Point(146, 144);
+            this.lblNumTriathlete.Name = "lblNumTriathlete";
+            this.lblNumTriathlete.Size = new System.Drawing.Size(139, 16);
+            this.lblNumTriathlete.TabIndex = 3;
+            this.lblNumTriathlete.Text = "Numéro de Triathlète :";
+            // 
+            // lblPrélèvement
+            // 
+            this.lblPrélèvement.AutoSize = true;
+            this.lblPrélèvement.Location = new System.Drawing.Point(147, 79);
+            this.lblPrélèvement.Name = "lblPrélèvement";
+            this.lblPrélèvement.Size = new System.Drawing.Size(141, 16);
+            this.lblPrélèvement.TabIndex = 2;
+            this.lblPrélèvement.Text = "Taux de Prélèvement :";
+            // 
+            // lblVerif
+            // 
+            this.lblVerif.AutoSize = true;
+            this.lblVerif.Location = new System.Drawing.Point(147, 115);
+            this.lblVerif.Name = "lblVerif";
+            this.lblVerif.Size = new System.Drawing.Size(49, 16);
+            this.lblVerif.TabIndex = 1;
+            this.lblVerif.Text = "Positif :";
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(147, 45);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(77, 16);
+            this.lblId.TabIndex = 0;
+            this.lblId.Text = "Id Contrôle :";
             // 
             // FormControle
             // 
@@ -74,6 +166,10 @@
             this.Text = "Contrôle Anti-Dopage";
             this.Load += new System.EventHandler(this.FormControle_Load);
             this.tabControleDopage.ResumeLayout(false);
+            this.tabPageAffDopage.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridDopage)).EndInit();
+            this.tabPageDetails.ResumeLayout(false);
+            this.tabPageDetails.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -83,5 +179,13 @@
         private System.Windows.Forms.TabControl tabControleDopage;
         private System.Windows.Forms.TabPage tabPageAffDopage;
         private System.Windows.Forms.TabPage tabPageDetails;
+        private System.Windows.Forms.DataGridView dataGridDopage;
+        private System.Windows.Forms.Label lblNumTriathlete;
+        private System.Windows.Forms.Label lblPrélèvement;
+        private System.Windows.Forms.Label lblVerif;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.Label lblCodeProduit;
+        private System.Windows.Forms.Label lblIdLabo;
+        private System.Windows.Forms.Label lblNumTriathlon;
     }
 }
--- Triathlon/FormTriathlon.Designer.cs
+++ Triathlon/FormTriathlon.Designer.cs
@@ -30,8 +30,23 @@
         {
             this.tabControlTriathlon = new System.Windows.Forms.TabControl();
             this.tabPageAffTriathlon = new System.Windows.Forms.TabPage();
+            this.dataGridTriathlon = new System.Windows.Forms.DataGridView();
             this.tabPageDetails = new System.Windows.Forms.TabPage();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblLieu = new System.Windows.Forms.Label();
+            this.lblNom = new System.Windows.Forms.Label();
+            this.lblNumero = new System.Windows.Forms.Label();
+            this.btnValider = new System.Windows.Forms.Button();
+            this.btnSupprimer = new System.Windows.Forms.Button();
+            this.btnAnnuler = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
             this.tabControlTriathlon.SuspendLayout();
+            this.tabPageAffTriathlon.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridTriathlon)).BeginInit();
+            this.tabPageDetails.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControlTriathlon
@@ -46,6 +61,7 @@
             // 
             // tabPageAffTriathlon
             // 
+            this.tabPageAffTriathlon.Controls.Add(this.dataGridTriathlon);
             this.tabPageAffTriathlon.Location = new System.Drawing.Point(4, 25);
             this.tabPageAffTriathlon.Name = "tabPageAffTriathlon";
             this.tabPageAffTriathlon.Padding = new System.Windows.Forms.Padding(3);
@@ -54,8 +70,29 @@
             this.tabPageAffTriathlon.Text = "Affichage Triathlon";
             this.tabPageAffTriathlon.UseVisualStyleBackColor = true;
             // 
+            // dataGridTriathlon
+            // 
+            this.dataGridTriathlon.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridTriathlon.Location = new System.Drawing.Point(6, 6);
+            this.dataGridTriathlon.Name = "dataGridTriathlon";
+            this.dataGridTriathlon.RowHeadersWidth = 51;
+            this.dataGridTriathlon.RowTemplate.Height = 24;
+            this.dataGridTriathlon.Size = new System.Drawing.Size(756, 385);
+            this.dataGridTriathlon.TabIndex = 1;
+            // 
             // tabPageDetails
             // 
+            this.tabPageDetails.Controls.Add(this.textBox4);
+            this.tabPageDetails.Controls.Add(this.textBox3);
+            this.tabPageDetails.Controls.Add(this.textBox2);
+            this.tabPageDetails.Controls.Add(this.textBox1);
+            this.tabPageDetails.Controls.Add(this.btnAnnuler);
+            this.tabPageDetails.Controls.Add(this.btnSupprimer);
+            this.tabPageDetails.Controls.Add(this.btnValider);
+            this.tabPageDetails.Controls.Add(this.lblDate);
+            this.tabPageDetails.Controls.Add(this.lblLieu);
+            this.tabPageDetails.Controls.Add(this.lblNom);
+            this.tabPageDetails.Controls.Add(this.lblNumero);
             this.tabPageDetails.Location = new System.Drawing.Point(4, 25);
             this.tabPageDetails.Name = "tabPageDetails";
             this.tabPageDetails.Padding = new System.Windows.Forms.Padding(3);
@@ -63,6 +100,101 @@
             this.tabPageDetails.TabIndex = 1;
             this.tabPageDetails.Text = "Détails";
             this.tabPageDetails.UseVisualStyleBackColor = true;
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(111, 188);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(139, 16);
+            this.lblDate.TabIndex = 3;
+            this.lblDate.Text = "Date de déroulement :";
+            // 
+            // lblLieu
+            // 
+            this.lblLieu.AutoSize = true;
+            this.lblLieu.Location = new System.Drawing.Point(124, 146);
+            this.lblLieu.Name = "lblLieu";
+            this.lblLieu.Size = new System.Drawing.Size(126, 16);
+            this.lblLieu.TabIndex = 2;
+            this.lblLieu.Text = "Lieu d\'organisation :";
+            // 
+            // lblNom
+            // 
+            this.lblNom.AutoSize = true;
+            this.lblNom.Location = new System.Drawing.Point(135, 101);
+            this.lblNom.Name = "lblNom";
+            this.lblNom.Size = new System.Drawing.Size(115, 16);
+            this.lblNom.TabIndex = 1;
+            this.lblNom.Text = "Nom du Triathlon :";
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Location = new System.Drawing.Point(134, 58);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(116, 16);
+            this.lblNumero.TabIndex = 0;
+            this.lblNumero.Text = "Numéro Triathlon :";
+            // 
+            // btnValider
+            // 
+            this.btnValider.BackColor = System.Drawing.Color.Lime;
+            this.btnValider.Location = new System.Drawing.Point(206, 270);
+            this.btnValider.Name = "btnValider";
+            this.btnValider.Size = new System.Drawing.Size(115, 32);
+            this.btnValider.TabIndex = 4;
+            this.btnValider.Text = "Valider";
+            this.btnValider.UseVisualStyleBackColor = false;
+            // 
+            // btnSupprimer
+            // 
+            this.btnSupprimer.BackColor = System.Drawing.Color.Red;
+            this.btnSupprimer.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSupprimer.Location = new System.Drawing.Point(472, 58);
+            this.btnSupprimer.Name = "btnSupprimer";
+            this.btnSupprimer.Size = new System.Drawing.Size(112, 32);
+            this.btnSupprimer.TabIndex = 5;
+            this.btnSupprimer.Text = "Supprimer";
+            this.btnSupprimer.UseVisualStyleBackColor = false;
+            // 
+            // btnAnnuler
+            // 
+            this.btnAnnuler.BackColor = System.Drawing.Color.LightCoral;
+            this.btnAnnuler.Location = new System.Drawing.Point(357, 270);
+            this.btnAnnuler.Name = "btnAnnuler";
+            this.btnAnnuler.Size = new System.Drawing.Size(106, 32);
+            this.btnAnnuler.TabIndex = 6;
+            this.btnAnnuler.Text = "Annuler";
+            this.btnAnnuler.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(257, 52);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(257, 94);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(257, 139);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(257, 181);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 10;
             // 
             // FormTriathlon
             // 
@@ -73,6 +205,10 @@
             this.Name = "FormTriathlon";
             this.Text = "Gestionnaire des Triathlon";
             this.tabControlTriathlon.ResumeLayout(false);
+            this.tabPageAffTriathlon.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridTriathlon)).EndInit();
+            this.tabPageDetails.ResumeLayout(false);
+            this.tabPageDetails.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -82,5 +218,17 @@
         private System.Windows.Forms.TabControl tabControlTriathlon;
         private System.Windows.Forms.TabPage tabPageAffTriathlon;
         private System.Windows.Forms.TabPage tabPageDetails;
+        private System.Windows.Forms.DataGridView dataGridTriathlon;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblLieu;
+        private System.Windows.Forms.Label lblNom;
+        private System.Windows.Forms.Label lblNumero;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnAnnuler;
+        private System.Windows.Forms.Button btnSupprimer;
+        private System.Windows.Forms.Button btnValider;
     }
 }
