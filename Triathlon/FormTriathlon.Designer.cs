d9bcbf2e3ce758b75adcc580dd3d891b5cd5b96c
Zully <135966635+Aezule@users.noreply.github.com>
2024-04-29 15:31:34 +0200

    BindingSource

--- Triathlon/FormTriathlon.Designer.cs
+++ Triathlon/FormTriathlon.Designer.cs
@@ -28,25 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.tabControlTriathlon = new System.Windows.Forms.TabControl();
             this.tabPageAffTriathlon = new System.Windows.Forms.TabPage();
             this.dataGridTriathlon = new System.Windows.Forms.DataGridView();
             this.tabPageDetails = new System.Windows.Forms.TabPage();
+            this.txtDate = new System.Windows.Forms.TextBox();
+            this.txtLieu = new System.Windows.Forms.TextBox();
+            this.txtNomTri = new System.Windows.Forms.TextBox();
+            this.txtNumTri = new System.Windows.Forms.TextBox();
+            this.btnAnnuler = new System.Windows.Forms.Button();
+            this.btnSupprimer = new System.Windows.Forms.Button();
+            this.btnValider = new System.Windows.Forms.Button();
             this.lblDate = new System.Windows.Forms.Label();
             this.lblLieu = new System.Windows.Forms.Label();
             this.lblNom = new System.Windows.Forms.Label();
             this.lblNumero = new System.Windows.Forms.Label();
-            this.btnValider = new System.Windows.Forms.Button();
-            this.btnSupprimer = new System.Windows.Forms.Button();
-            this.btnAnnuler = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.numTriathlonDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lieuDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nomDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dateTriathlonDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.codeTypeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.iNSCRIPTIONsDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tYPETRIATHLONDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TriathlonBinding = new System.Windows.Forms.BindingSource(this.components);
             this.tabControlTriathlon.SuspendLayout();
             this.tabPageAffTriathlon.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridTriathlon)).BeginInit();
             this.tabPageDetails.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.TriathlonBinding)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControlTriathlon
@@ -72,20 +82,36 @@
             // 
             // dataGridTriathlon
             // 
+            this.dataGridTriathlon.AllowUserToAddRows = false;
+            this.dataGridTriathlon.AllowUserToDeleteRows = false;
+            this.dataGridTriathlon.AutoGenerateColumns = false;
             this.dataGridTriathlon.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridTriathlon.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.numTriathlonDataGridViewTextBoxColumn,
+            this.lieuDataGridViewTextBoxColumn,
+            this.nomDataGridViewTextBoxColumn,
+            this.dateTriathlonDataGridViewTextBoxColumn,
+            this.codeTypeDataGridViewTextBoxColumn,
+            this.iNSCRIPTIONsDataGridViewTextBoxColumn,
+            this.tYPETRIATHLONDataGridViewTextBoxColumn});
+            this.dataGridTriathlon.DataSource = this.TriathlonBinding;
             this.dataGridTriathlon.Location = new System.Drawing.Point(6, 6);
+            this.dataGridTriathlon.MultiSelect = false;
             this.dataGridTriathlon.Name = "dataGridTriathlon";
+            this.dataGridTriathlon.ReadOnly = true;
             this.dataGridTriathlon.RowHeadersWidth = 51;
             this.dataGridTriathlon.RowTemplate.Height = 24;
+            this.dataGridTriathlon.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridTriathlon.Size = new System.Drawing.Size(756, 385);
             this.dataGridTriathlon.TabIndex = 1;
+            this.dataGridTriathlon.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridTriathlon_CellMouseClick);
             // 
             // tabPageDetails
             // 
-            this.tabPageDetails.Controls.Add(this.textBox4);
-            this.tabPageDetails.Controls.Add(this.textBox3);
-            this.tabPageDetails.Controls.Add(this.textBox2);
-            this.tabPageDetails.Controls.Add(this.textBox1);
+            this.tabPageDetails.Controls.Add(this.txtDate);
+            this.tabPageDetails.Controls.Add(this.txtLieu);
+            this.tabPageDetails.Controls.Add(this.txtNomTri);
+            this.tabPageDetails.Controls.Add(this.txtNumTri);
             this.tabPageDetails.Controls.Add(this.btnAnnuler);
             this.tabPageDetails.Controls.Add(this.btnSupprimer);
             this.tabPageDetails.Controls.Add(this.btnValider);
@@ -100,6 +126,71 @@
             this.tabPageDetails.TabIndex = 1;
             this.tabPageDetails.Text = "Détails";
             this.tabPageDetails.UseVisualStyleBackColor = true;
+            // 
+            // txtDate
+            // 
+            this.txtDate.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.TriathlonBinding, "dateTriathlon", true));
+            this.txtDate.Location = new System.Drawing.Point(257, 181);
+            this.txtDate.Name = "txtDate";
+            this.txtDate.Size = new System.Drawing.Size(188, 22);
+            this.txtDate.TabIndex = 10;
+            // 
+            // txtLieu
+            // 
+            this.txtLieu.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.TriathlonBinding, "lieu", true));
+            this.txtLieu.Location = new System.Drawing.Point(257, 139);
+            this.txtLieu.Name = "txtLieu";
+            this.txtLieu.Size = new System.Drawing.Size(188, 22);
+            this.txtLieu.TabIndex = 9;
+            // 
+            // txtNomTri
+            // 
+            this.txtNomTri.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.TriathlonBinding, "nom", true));
+            this.txtNomTri.Location = new System.Drawing.Point(257, 95);
+            this.txtNomTri.Name = "txtNomTri";
+            this.txtNomTri.Size = new System.Drawing.Size(256, 22);
+            this.txtNomTri.TabIndex = 8;
+            // 
+            // txtNumTri
+            // 
+            this.txtNumTri.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.TriathlonBinding, "numTriathlon", true));
+            this.txtNumTri.Enabled = false;
+            this.txtNumTri.Location = new System.Drawing.Point(257, 52);
+            this.txtNumTri.Name = "txtNumTri";
+            this.txtNumTri.Size = new System.Drawing.Size(64, 22);
+            this.txtNumTri.TabIndex = 7;
+            // 
+            // btnAnnuler
+            // 
+            this.btnAnnuler.BackColor = System.Drawing.Color.LightCoral;
+            this.btnAnnuler.Location = new System.Drawing.Point(357, 270);
+            this.btnAnnuler.Name = "btnAnnuler";
+            this.btnAnnuler.Size = new System.Drawing.Size(106, 32);
+            this.btnAnnuler.TabIndex = 6;
+            this.btnAnnuler.Text = "Annuler";
+            this.btnAnnuler.UseVisualStyleBackColor = false;
+            // 
+            // btnSupprimer
+            // 
+            this.btnSupprimer.BackColor = System.Drawing.Color.Red;
+            this.btnSupprimer.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSupprimer.Location = new System.Drawing.Point(540, 270);
+            this.btnSupprimer.Name = "btnSupprimer";
+            this.btnSupprimer.Size = new System.Drawing.Size(112, 32);
+            this.btnSupprimer.TabIndex = 5;
+            this.btnSupprimer.Text = "Supprimer";
+            this.btnSupprimer.UseVisualStyleBackColor = false;
+            // 
+            // btnValider
+            // 
+            this.btnValider.BackColor = System.Drawing.Color.Lime;
+            this.btnValider.Location = new System.Drawing.Point(206, 270);
+            this.btnValider.Name = "btnValider";
+            this.btnValider.Size = new System.Drawing.Size(115, 32);
+            this.btnValider.TabIndex = 4;
+            this.btnValider.Text = "Valider";
+            this.btnValider.UseVisualStyleBackColor = false;
+            this.btnValider.Click += new System.EventHandler(this.btnValider_Click);
             // 
             // lblDate
             // 
@@ -137,64 +228,72 @@
             this.lblNumero.TabIndex = 0;
             this.lblNumero.Text = "Numéro Triathlon :";
             // 
-            // btnValider
+            // numTriathlonDataGridViewTextBoxColumn
             // 
-            this.btnValider.BackColor = System.Drawing.Color.Lime;
-            this.btnValider.Location = new System.Drawing.Point(206, 270);
-            this.btnValider.Name = "btnValider";
-            this.btnValider.Size = new System.Drawing.Size(115, 32);
-            this.btnValider.TabIndex = 4;
-            this.btnValider.Text = "Valider";
-            this.btnValider.UseVisualStyleBackColor = false;
+            this.numTriathlonDataGridViewTextBoxColumn.DataPropertyName = "numTriathlon";
+            this.numTriathlonDataGridViewTextBoxColumn.HeaderText = "numTriathlon";
+            this.numTriathlonDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.numTriathlonDataGridViewTextBoxColumn.Name = "numTriathlonDataGridViewTextBoxColumn";
+            this.numTriathlonDataGridViewTextBoxColumn.ReadOnly = true;
+            this.numTriathlonDataGridViewTextBoxColumn.Width = 125;
             // 
-            // btnSupprimer
+            // lieuDataGridViewTextBoxColumn
             // 
-            this.btnSupprimer.BackColor = System.Drawing.Color.Red;
-            this.btnSupprimer.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnSupprimer.Location = new System.Drawing.Point(472, 58);
-            this.btnSupprimer.Name = "btnSupprimer";
-            this.btnSupprimer.Size = new System.Drawing.Size(112, 32);
-            this.btnSupprimer.TabIndex = 5;
-            this.btnSupprimer.Text = "Supprimer";
-            this.btnSupprimer.UseVisualStyleBackColor = false;
+            this.lieuDataGridViewTextBoxColumn.DataPropertyName = "lieu";
+            this.lieuDataGridViewTextBoxColumn.HeaderText = "lieu";
+            this.lieuDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.lieuDataGridViewTextBoxColumn.Name = "lieuDataGridViewTextBoxColumn";
+            this.lieuDataGridViewTextBoxColumn.ReadOnly = true;
+            this.lieuDataGridViewTextBoxColumn.Width = 125;
             // 
-            // btnAnnuler
+            // nomDataGridViewTextBoxColumn
             // 
-            this.btnAnnuler.BackColor = System.Drawing.Color.LightCoral;
-            this.btnAnnuler.Location = new System.Drawing.Point(357, 270);
-            this.btnAnnuler.Name = "btnAnnuler";
-            this.btnAnnuler.Size = new System.Drawing.Size(106, 32);
-            this.btnAnnuler.TabIndex = 6;
-            this.btnAnnuler.Text = "Annuler";
-            this.btnAnnuler.UseVisualStyleBackColor = false;
+            this.nomDataGridViewTextBoxColumn.DataPropertyName = "nom";
+            this.nomDataGridViewTextBoxColumn.HeaderText = "nom";
+            this.nomDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.nomDataGridViewTextBoxColumn.Name = "nomDataGridViewTextBoxColumn";
+            this.nomDataGridViewTextBoxColumn.ReadOnly = true;
+            this.nomDataGridViewTextBoxColumn.Width = 125;
             // 
-            // textBox1
+            // dateTriathlonDataGridViewTextBoxColumn
             // 
-            this.textBox1.Location = new System.Drawing.Point(257, 52);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 7;
+            this.dateTriathlonDataGridViewTextBoxColumn.DataPropertyName = "dateTriathlon";
+            this.dateTriathlonDataGridViewTextBoxColumn.HeaderText = "dateTriathlon";
+            this.dateTriathlonDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.dateTriathlonDataGridViewTextBoxColumn.Name = "dateTriathlonDataGridViewTextBoxColumn";
+            this.dateTriathlonDataGridViewTextBoxColumn.ReadOnly = true;
+            this.dateTriathlonDataGridViewTextBoxColumn.Width = 125;
             // 
-            // textBox2
+            // codeTypeDataGridViewTextBoxColumn
             // 
-            this.textBox2.Location = new System.Drawing.Point(257, 94);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
-            this.textBox2.TabIndex = 8;
+            this.codeTypeDataGridViewTextBoxColumn.DataPropertyName = "codeType";
+            this.codeTypeDataGridViewTextBoxColumn.HeaderText = "codeType";
+            this.codeTypeDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.codeTypeDataGridViewTextBoxColumn.Name = "codeTypeDataGridViewTextBoxColumn";
+            this.codeTypeDataGridViewTextBoxColumn.ReadOnly = true;
+            this.codeTypeDataGridViewTextBoxColumn.Width = 125;
             // 
-            // textBox3
+            // iNSCRIPTIONsDataGridViewTextBoxColumn
             // 
-            this.textBox3.Location = new System.Drawing.Point(257, 139);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 22);
-            this.textBox3.TabIndex = 9;
+            this.iNSCRIPTIONsDataGridViewTextBoxColumn.DataPropertyName = "INSCRIPTIONs";
+            this.iNSCRIPTIONsDataGridViewTextBoxColumn.HeaderText = "INSCRIPTIONs";
+            this.iNSCRIPTIONsDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.iNSCRIPTIONsDataGridViewTextBoxColumn.Name = "iNSCRIPTIONsDataGridViewTextBoxColumn";
+            this.iNSCRIPTIONsDataGridViewTextBoxColumn.ReadOnly = true;
+            this.iNSCRIPTIONsDataGridViewTextBoxColumn.Width = 125;
             // 
-            // textBox4
+            // tYPETRIATHLONDataGridViewTextBoxColumn
             // 
-            this.textBox4.Location = new System.Drawing.Point(257, 181);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 22);
-            this.textBox4.TabIndex = 10;
+            this.tYPETRIATHLONDataGridViewTextBoxColumn.DataPropertyName = "TYPE_TRIATHLON";
+            this.tYPETRIATHLONDataGridViewTextBoxColumn.HeaderText = "TYPE_TRIATHLON";
+            this.tYPETRIATHLONDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.tYPETRIATHLONDataGridViewTextBoxColumn.Name = "tYPETRIATHLONDataGridViewTextBoxColumn";
+            this.tYPETRIATHLONDataGridViewTextBoxColumn.ReadOnly = true;
+            this.tYPETRIATHLONDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // TriathlonBinding
+            // 
+            this.TriathlonBinding.DataSource = typeof(Triathlon.TRIATHLON);
             // 
             // FormTriathlon
             // 
@@ -204,11 +303,13 @@
             this.Controls.Add(this.tabControlTriathlon);
             this.Name = "FormTriathlon";
             this.Text = "Gestionnaire des Triathlon";
+            this.Load += new System.EventHandler(this.FormTriathlon_Load);
             this.tabControlTriathlon.ResumeLayout(false);
             this.tabPageAffTriathlon.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridTriathlon)).EndInit();
             this.tabPageDetails.ResumeLayout(false);
             this.tabPageDetails.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.TriathlonBinding)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -223,12 +324,20 @@
         private System.Windows.Forms.Label lblLieu;
         private System.Windows.Forms.Label lblNom;
         private System.Windows.Forms.Label lblNumero;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtDate;
+        private System.Windows.Forms.TextBox txtLieu;
+        private System.Windows.Forms.TextBox txtNomTri;
+        private System.Windows.Forms.TextBox txtNumTri;
         private System.Windows.Forms.Button btnAnnuler;
         private System.Windows.Forms.Button btnSupprimer;
         private System.Windows.Forms.Button btnValider;
+        private System.Windows.Forms.DataGridViewTextBoxColumn numTriathlonDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lieuDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nomDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dateTriathlonDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn codeTypeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn iNSCRIPTIONsDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tYPETRIATHLONDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource TriathlonBinding;
     }
 }
--- Triathlon/FormTriathlon.cs
+++ Triathlon/FormTriathlon.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Data.Entity;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,51 @@ namespace Triathlon
         public FormTriathlon()
         {
             InitializeComponent();
+        }
+        TriathlonEntities context;
+
+        private void FormTriathlon_Load(object sender, EventArgs e)
+        {
+            try
+            {
+
+                context = new TriathlonEntities();
+                context.TRIATHLONs.Load();
+           
+                TriathlonBinding.DataSource = context.TRIATHLONs.Local.ToBindingList();
+    
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
+        private void dataGridTriathlon_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            TRIATHLON leTriathlon = (TRIATHLON)TriathlonBinding.Current;
+            tabControlTriathlon.SelectedIndex = 1;
+        }
+
+        private void btnValider_Click(object sender, EventArgs e)
+        {
+           /*
+            
+            Erreur voir plus tard
+             
+            TRIATHLON unTL = (TRIATHLON)TriathlonBinding.Current;
+            try
+            {  
+                TriathlonBinding.EndEdit();
+                context.TRIATHLONs.Add(unTL);
+                context.SaveChanges();
+                TriathlonBinding.ResetBindings(false);
+
+            }
+            catch (Exception ex)
+            {
+
+                context.Entry((TRIATHLON)TriathlonBinding.Current).State = EntityState.Unchanged;
+                MessageBox.Show(ex.Message, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+           */
         }
     }
 }
